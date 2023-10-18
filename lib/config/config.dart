3b795a6d726c7703ebb3262f8d64ee93a37aeec1
shreyxnsh <shreyanshatwork@gmail.com>
2023-10-18 15:22:34 +0530

    http package added

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,4 +1,4 @@
 // 192.168.56.1
 final url = 'http://192.168.56.1:3000/';
-final registration = url + 'registration';
-final login = url + 'login';
+final registrationUrl = url + 'registration';
+final loginUrl = url + 'login';
